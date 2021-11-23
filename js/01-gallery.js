c5def02cc117192015c282dc7020652195fbff18
Taras-Melnyk1987 <dr.melnuk1987@gmail.com>
2021-11-23 06:00:06 +0200

    add task 1, 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.js-gallery');
+gallery.addEventListener('click', onModalOpen);
+
+//разметка
+function createGalleryItems(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt='${description}'
+          />
+        </a>
+      </div>`,
+    )
+    .join('');
+}
+const galleryImages = createGalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+let instance = '';
+
+//для inctance
+const options = {
+  once: true,
+  onShow: instance => {
+    window.addEventListener('keydown', eventHandler);
+  },
+
+  onClose: instance => {
+    window.removeEventListener('keydown', eventHandler);
+  },
+};
+function eventHandler(e) {
+  if (e.key === 'Escape') {
+    instance.close();
+    return;
+  }
+}
+//открытие модалки
+function onModalOpen(e) {
+  e.preventDefault();
+
+  const targetImg = e.target.nodeName === 'IMG';
+  if (!targetImg) return;
+
+  const url = getImageUrl(e.target);
+
+  instance = basicLightbox.create(
+    `<img src="${url}" width="800" height="600">`,
+    options,
+  );
+
+  instance.show();
+}
+
+// функция image
+function getImageUrl(image) {
+  return image.dataset.source;
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryImages = createGalleryItems(galleryItems);
+// gallery.addEventListener('click', onModalOpen);
+
+function createGalleryItems(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="item"><a class="gallery__item" style='display: inline-block; height: 240px' href=${original}>
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a></li>`,
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
